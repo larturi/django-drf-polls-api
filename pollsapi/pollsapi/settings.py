c19ce55be9f0fca48f1953f4296775f02cc93c26
lea.arturi <lea.arturi@gmail.com>
2021-09-13 17:37:32 -0300

    Swagger

--- pollsapi/pollsapi/settings.py
+++ pollsapi/pollsapi/settings.py
@@ -26,8 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
+
     'polls',
 ]
 
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pollsapi/pollsapi/urls.py
+++ pollsapi/pollsapi/urls.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import include, re_path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Polls API",
+        default_version='v1',
+        description="Polls API Docs",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="lea.arturi@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('docs/', schema_view.with_ui('swagger',
+                                      cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc',
+                                        cache_timeout=0), name='schema-redoc'),
+
     re_path(r'^', include('polls.urls')),
+
 ]
